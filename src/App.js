e7b8f58bb462de25a8f7a1ffd0571370865eca5f
joyjsmun <joy.js.mun@gmail.com>
2022-04-11 16:50:37 +0900

    added handleChange fn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import React,{useState} from "react"
 
-export default function App (){
-    return(
-      <div>
-      <h1>How fast do you type?</h1>
-      <textarea />
-      <h4>Time reminaing: ???</h4>
-      <button>Start</button>
-      <h1>Word count: ???</h1>
-  </div>
+function App() {
+    const [text,setText] = useState("")
+    
+    function handleChange(e) {
+      const {value} = e.target
+        setText(value)
+    }
+    
+    console.log(text)
+    
+    return (
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea value={text} onChange={handleChange}>
+            {text}
+            </textarea>
+            <h4>Time reminaing: ???</h4>
+            <button>Start</button>
+            <h1>Word count: ???</h1>
+        </div>
     )
 }
+
+export default App
