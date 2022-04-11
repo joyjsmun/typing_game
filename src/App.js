60086b6c77356f5c58c6bb2c8fa324b437fda498
joyjsmun <joy.js.mun@gmail.com>
2022-04-11 16:33:47 +0900

    added first structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
-
-function App() {
-  return (
-   <div>test</div>
-  )
+export default function App (){
+    return(
+      <div>
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h4>Time reminaing: ???</h4>
+      <button>Start</button>
+      <h1>Word count: ???</h1>
+  </div>
+    )
 }
-
-export default App;
