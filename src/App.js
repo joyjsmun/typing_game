a82d4c77ea37c03f2132291fdab75f7d6ed90f95
joyjsmun <joy.js.mun@gmail.com>
2022-04-11 22:23:05 +0900

    added counting words fn

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React,{useState} from "react"
 
 function App() {
     const [text,setText] = useState("")
+    const [count, setCount] = useState(0)
+    
+    function countWord(event){
+       setCount((text.split(' ').length))
+    }
+    
     
     function handleChange(e) {
       const {value} = e.target
         setText(value)
     }
     
-    console.log(text)
     
     return (
         <div>
@@ -17,8 +22,8 @@ function App() {
             {text}
             </textarea>
             <h4>Time reminaing: ???</h4>
-            <button>Start</button>
-            <h1>Word count: ???</h1>
+            <button onClick={countWord}>Start</button>
+            <h1>Word count: {count}</h1>
         </div>
     )
 }
